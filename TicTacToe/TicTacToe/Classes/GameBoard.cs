ff8878a770cfd5334bff41ee3891a6564240ae53
jcqnly <jcqnly@gmail.com>
2018-06-11 19:05:56 -0700

    instantiate a gameboard and display it

--- TicTacToe/TicTacToe/Classes/GameBoard.cs
+++ TicTacToe/TicTacToe/Classes/GameBoard.cs
@@ -6,6 +6,11 @@ namespace TicTacToe.Classes
 {
     class GameBoard
     {
-
+        public GameBoard()
+        {
+            Console.WriteLine(" | 1 |  | 2 |  | 3 |\n" +
+                " | 4 |  | 5 |  | 6 |\n" +
+                " | 7 |  | 8 |  | 8 |\n");
+        }
     }
 }
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -7,22 +7,38 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TIC TAC TOE!");
+            Console.WriteLine("!!!TIC TAC TOE!!!");
             PlayerChoose();
             Console.Read();
             Console.Clear();
+            DisplayGame();
+            Console.Read();
         }
 
+        /// <summary>
+        /// Instantiate new players and set the values for their names and markers
+        /// </summary>
         public static void PlayerChoose()
         {
             Console.WriteLine("Player 1, enter your name: ");
+            //create an instance of the Player class and set values for player 1
             Player Player1 = new Player(Console.ReadLine(), "X");
             
             Console.WriteLine("Player 2, enter your name: ");
+            //create an instance of the Player class and set values for player 1
             Player Player2 = new Player(Console.ReadLine(), "O");
 
             Console.WriteLine($"Player 1 is {Player1.Name}, and you are {Player1.Marker}");
             Console.WriteLine($"Player 2 is {Player2.Name}, and you are {Player2.Marker}");
+        }
+
+        /// <summary>
+        /// Instantiate a gameboard to display
+        /// </summary>
+        public static void DisplayGame()
+        {
+            GameBoard game = new GameBoard();
+            PlayerActions();
         }
     }
 }
