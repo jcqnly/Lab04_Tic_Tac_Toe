0caa44d0d9e57cec69a764eea8b416a2a8e4430d
jcqnly <jcqnly@gmail.com>
2018-06-11 18:51:52 -0700

    establish Player class

---
+++ TicTacToe/TicTacToe/Classes/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Classes
+{
+    class Player
+    {   //get the name from the user input and store it
+        public string Name { get; set; }
+        //set the Marker: X or O for each user
+        public string Marker { get; set; }
+
+        public Player(string name, string marker)
+        {
+            Name = name;
+            Marker = marker;
+        }
+
+
+    }
+}
